6e98e21c49cd93b1eb93fc969938ba3c0a5f0c1b
Ucha <usamathpallikkachalil@gmail.com>
2019-08-03 17:23:34 +0530

    added max clicked cards=2 and close (if no match)

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
  *putting that inside array-braces creates an array with a single element which is the NodeList
  *using the ... prefix transfers the NodeList into individual items in the array.
  */
-let allCards = [...document.querySelectorAll(".deck li")];
+let allCards = [ ...document.querySelectorAll('.deck li') ];
 
 /*
  * Display the cards on the page
@@ -15,69 +15,81 @@ let allCards = [...document.querySelectorAll(".deck li")];
 
 // createHTML to shuffle and reset cards
 function shuffleAndResetHTML() {
-  // create array of existing cards from the present deck
-  let allCards = [...document.querySelectorAll(".deck li")];
+	// create array of existing cards from the present deck
+	let allCards = [ ...document.querySelectorAll('.deck li') ];
 
-  // shuffle it
-  let shuffledCards = shuffle(allCards);
+	// shuffle it
+	let shuffledCards = shuffle(allCards);
 
-  // convert it into HTML to display it
-  let newDeck = shuffledCards[0].outerHTML;
-  shuffledCards.forEach(function(el, index) {
-    if (index < 1) return;
-    newDeck += `${el.outerHTML}`;
-  });
-  document.querySelector(".deck").innerHTML = newDeck;
+	// convert it into HTML to display it
+	let newDeck = shuffledCards[0].outerHTML;
+	shuffledCards.forEach(function(el, index) {
+		if (index < 1) return;
+		newDeck += `${el.outerHTML}`;
+	});
+	document.querySelector('.deck').innerHTML = newDeck;
 
-  // turn cards OFF
-  resetCards();
+	// turn cards OFF
+	resetCards();
 }
 
 // reset cards in the existing deck
 function resetCards() {
-  let cardsToReset = [...document.querySelectorAll(".deck li")];
-  cardsToReset.forEach(function(el) {
-    el.classList.remove("open", "show", "match");
-  });
+	let cardsToReset = [ ...document.querySelectorAll('.deck li') ];
+	cardsToReset.forEach(function(el) {
+		el.classList.remove('open', 'show', 'match');
+	});
 }
 
 //function to show all cards in the existing deck
 function showAllCards() {
-  let cardsToShow = [...document.querySelectorAll(".deck li")];
-  cardsToShow.forEach(function(el) {
-    el.classList.add("show", "open");
-  });
+	let cardsToShow = [ ...document.querySelectorAll('.deck li') ];
+	cardsToShow.forEach(function(el) {
+		el.classList.add('show', 'open');
+	});
 }
 
 // show clicked card
-var count = 0;
+var clickedCard = [];
 function showClickedCard(evt) {
-  count++;
-  if (count <= 2) {
-    evt.target.classList.add("show", "open");
-    setTimeout(function() {
-      evt.target.classList.remove("show", "open");
-    }, 3000);
-  }
+	clickedCard.push(evt.target);
+	console.log(clickedCard);
+	if (clickedCard.length <= 2) {
+		clickedCard[`${clickedCard.length - 1}`].classList.add('show', 'open');
+	} else if (clickedCard.length > 2) {
+		timeOut();
+		clickedCard = [];
+	}
+}
+function timeOut() {
+	console.log(clickedCard);
+	clickedCard[0].classList.remove('show', 'open');
+	clickedCard[1].classList.remove('show', 'open');
 }
 
-document.addEventListener("click", showClickedCard);
+shuffleAndResetHTML();
+setTimeout(showAllCards, 2000);
+setTimeout(resetCards, 5000);
+var listenForClicks = document.querySelectorAll('.deck li:not(.show):not(.open):not(.match)');
+listenForClicks.forEach(function(el) {
+	el.addEventListener('click', showClickedCard);
+});
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
+	var currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
 
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-  return array;
+	return array;
 }
 
 /*
